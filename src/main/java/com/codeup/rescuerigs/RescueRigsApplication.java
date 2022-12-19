5b7fca4b64319b83d5bd3516ed44c80e4d87f06f
Oscar Castro <oscar.a.castro818@gmail.com>
2022-12-19 14:38:51 -0600

    initial setup

---
+++ src/main/java/com/codeup/rescuerigs/RescueRigsApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.rescuerigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescueRigsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RescueRigsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/rescuerigs/RescueRigsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.rescuerigs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueRigsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
